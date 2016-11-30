f70771c94a9c929e6095f5ecbec1754cf96d0e8f
Guzman Monne <guzmonne@hotmail.com>
2016-11-30 00:14:53 -0300

    updated App component to test ui-fabric

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {Fabric} from 'office-ui-fabric-react/lib/Fabric'
+import {Button, ButtonType} from 'office-ui-fabric-react/lib/Button'
+import {Dialog, DialogType, DialogFooter} from 'office-ui-fabric-react/lib/Dialog'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  open = () => this.setState({isOpen: true})
+
+  close = () => this.setState({isOpen: false})
+
   render() {
     return (
-      <div className="App">
-        <h1>Office UI Fabric</h1>
-      </div>
-    );
+      <Fabric className="App">
+        <div style={{margin: '5em'}}>
+          <Button onClick={this.open}>I am a button.</Button>
+        </div>
+        <Dialog
+          isOpen={this.state.isOpen}
+          type={DialogType.close}
+          onDismiss={this.close.bind(this)}
+          title='Dialog title'
+          subText='Dialog subText'
+          isBlocking={false}
+          closeButtonAriaLabel='Close'
+        >
+          <h1>Hello, World!</h1>
+          <DialogFooter>
+            <Button buttonType={ButtonType.primary} onClick={this.close}>OK</Button>
+          </DialogFooter>
+        </Dialog>
+      </Fabric>  
+    )
   }
 }
 
